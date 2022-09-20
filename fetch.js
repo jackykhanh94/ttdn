538f5fd45e2d116530f41bc5e547bbb0262df82c
jackykhanh94 <jackykhanh94@gmail.com>
2022-09-20 16:11:11 +0000

    bin ttdn

---
+++ fetch.js
@@ -0,0 +1,76 @@
+var async = require('async')
+var glob = require('glob')
+var cp = require('child_process')
+var colors = require('./colors')
+const { writeFileSync, appendFileSync, appendFile, writeFile, readdirSync, lstatSync } = require('fs')
+const path = require('path')
+var reqUrl = 'https://thongtindoanhnghiep.co/api/company?l={path}&r=100&p={page}'
+
+function getAllJsonFiles(dataPath) {
+    var files = []
+    if (path.extname(dataPath) == '.json') files = [dataPath]
+    else files = glob.sync(dataPath + '/**/*.json')
+    return files
+}
+
+async function fetchPage(lpath, page, retry = 0) {
+    try {
+        var cmd = `curl -sL "${reqUrl.replace(`{path}`, lpath).replace('{page}', page)}"`;
+        console.log(colors.toGreen('    [fetchPage]:'), colors.toYellow(cmd))
+        var output = cp.execSync(cmd)
+        return JSON.parse(output.toString())
+    } catch (e) {
+        ++retry
+        console.log(colors.toGreen('    [fetchPage][Retry]:'), retry, colors.toYellow(cmd))
+        if (retry < 10) return fetchPage(lpath, page, retry)
+        throw e;
+    }
+}
+async function fetchUtilLast(lpath) {
+    var page = 1;
+    var finish = false;
+    var result = [];
+    console.log(colors.toBlue('  [fetchUtilLast][START]:'), lpath)
+    while (!finish) {
+        var data = await fetchPage(lpath, page)
+        var { Option: { RowPerPage, CurrentPage, TotalRow }, LtsItems } = data;
+        finish = RowPerPage * CurrentPage >= TotalRow
+        page++;
+        console.log(colors.toBlue('  [fetchUtilLast][NEXT]:'), page, lpath, { RowPerPage, CurrentPage, TotalRow })
+        result.push(...LtsItems)
+    }
+    console.log(colors.toBlue('  [fetchUtilLast][FINISH]:'), lpath)
+    return result.map(f => clean(f));
+}
+function clean(obj) {
+    for (var propName in obj) {
+        if (obj[propName] === null || obj[propName] === undefined || obj[propName].length === 0 || obj[propName] === '') {
+            delete obj[propName];
+        }
+    }
+    return obj
+}
+module.exports = async function (dataPath) {
+    var files = getAllJsonFiles(dataPath)
+    var tasks = files.map((f) => {
+        var ward = require(f)
+        return (callback) => {
+            fetchUtilLast(ward.SolrID)
+                .then((result) => {
+                    var newWard = {
+                        ...ward,
+                        List: result
+                    }
+                    writeFile(dataPath, JSON.stringify(newWard), (err) => {
+                        console.log('  SAVE', dataPath)
+                        if (err) return callback(err)
+                        callback(null, newWard)
+                    })
+                })
+                .catch(callback)
+        }
+    })
+
+    await async.parallelLimit(tasks, 50)
+    console.log('DONE', dataPath)
+}
---
+++ ttdn.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+var colors = require('./colors')
+var fetch = require('./fetch')
+var fs = require('fs')
+const { spawn } = require('child_process')
+require('yargs')
+  .scriptName("ttdn")
+  .usage('$0 <cmd> [args]')
+  .command('dl [tinhthanh]', 'Tải thông tin doanh nghiệp theo vùng!', (yargs) => {
+    yargs.positional('tinhthanh', {
+      type: 'string',
+      describe: 'tinh-thanh hoặc tinh-thanh/quan-huyen hoặc tinh-thanh/quan-huyen/xa-phuong'
+    })
+  }, function (argv) {
+    var { tinhthanh } = argv
+    var parts = tinhthanh.split('/').filter(f => !!f)
+    var dataPath = './data/'
+    if (parts.length == 0) dataPath += parts[0]
+    else if (parts.length == 1) dataPath += parts.join('/')
+    else if (parts.length == 2) {
+        dataPath += parts.join('/')
+    }
+    else if (parts.length == 3) {
+        dataPath += parts.join('/')
+    }
+    else {
+        dataPath = tinhthanh
+    }
+    if (!/\.json$/.test(dataPath) && !fs.existsSync(dataPath)) {
+        dataPath += '.json';
+    }
+    if(!fs.existsSync(dataPath)) {
+        return console.log(colors.toRed('NOT FOUND:'), colors.toYellow(dataPath))
+    }
+    console.log(colors.toGreen('Download:'), colors.toMagenta(tinhthanh))
+    fetch(dataPath)
+  })
+  .command('init', 'Init data', (yargs) => {
+    
+  }, (args) => {
+    var proc = spawn('unzip', ['-o', 'data.zip', '-d', '.'], { stdio: 'pipe' })
+    proc.stdout.on('data', function (data) {
+      console.log(data.toString());
+    });
+    
+    proc.stderr.on('data', function (data) {
+      console.error(data.toString());
+    });
+    
+    proc.on('exit', function (code) {
+      console.log('Exit');
+    });
+  })
+  .help()
+  .argv
