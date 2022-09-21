93254bf47c1c712db2059c26ccda5dab6c3c4775
jackykhanh94 <jackykhanh94@gmail.com>
2022-09-21 09:57:19 +0000

    load company

--- company.js
+++ company.js
@@ -7,43 +7,63 @@ const { glob } = require('glob')
 var clean = require('./clean')
 var reqUrl = 'https://thongtindoanhnghiep.co/api/company/{mst}'
 
-var files = glob.sync('./data/*/**/*.json').slice(0, 100)
-async.parallelLimit(files.map((f) => {
-    return (callback) => {
-        // console.log('Load', f)
-        var { List } = require(f)
-        callback(null, List.map(f => f.Title))
-    }
-}), 10, (err, result) => {
-    // console.log(result.flat())
-})
-
 ;(!existsSync('./data/cache') && writeFileSync('./data/cache', ''))
 
 const Cache = readFileSync('./data/cache', 'utf-8').split('\n').filter(f => !!f).reduce((prev, cur) => {
     prev[cur] = true;
     return prev
 }, {}) || {}
-console.log('CACHE', Cache)
+// console.log('CACHE', Cache)
 async function save(mst, data) {
     appendFileSync('./data/companydb.json', JSON.stringify(data) + '\n')
     Cache[mst] = true;
     appendFileSync('./data/cache', mst + '\n')
     return data;
 }
-async function fetchCompany(mst) {
+var fetchCount = 0;
+async function fetchCompany(mst, retry = 0) {
     try {
-        if (Cache[mst]) return console.log('Ignore, company downloaded!')
+        if (retry == 0) fetchCount++
+        if (Cache[mst]) return console.log(colors.toGreen('[Load]:'), 'Ignore, company downloaded!', colors.toMagenta(`[${fetchCount}]`))
         var cmd = `curl -sL "${reqUrl.replace('{mst}',mst)}"`;
-        console.log(colors.toGreen('    [fetchCompany]:'), colors.toYellow(cmd))
+        console.log(colors.toGreen('[Load]:'), colors.toYellow(cmd), colors.toMagenta(`[${fetchCount}]`))
         var output = cp.execSync(cmd)
         var data = JSON.parse(output.toString())
         save(mst, data)
         return data
     } catch (e) {
         ++retry
-        console.log(colors.toGreen('    [fetchCompany][Retry]:'), retry, colors.toYellow(cmd))
-        if (retry < 10) return fetchCompany(lpath, page, retry)
+        console.log(colors.toGreen('[Load][Retry]:'), retry, colors.toYellow(cmd), colors.toMagenta(`[${fetchCount}]`))
+        if (retry < 10) return fetchCompany(mst, retry)
         throw e;
     }
 }
+
+;(async() => {
+    console.log('Download from', process.argv[2], 'to', process.argv[3])
+    var files = glob.sync('./data/*/**/*.json').slice(process.argv[2], process.argv[3]);
+    writeFileSync('queque-' + process.argv[2] + '-' + process.argv[3], new Date().toString())
+    var count = 0;
+    var tasks = async.parallelLimit(files.map((f) => {
+        return (callback) => {
+            var { List } = require(f)
+            async.parallelLimit(List.map(cmp => {
+                return (callback2) => {
+                    var mst = cmp.MaSoThue
+                    fetchCompany(mst)
+                        .then((res) => {
+                            count++
+                            console.log(colors.toGreen('[Done]:'), colors.toYellow(mst), colors.toMagenta(`[${count}/${process.argv[3]}]`))
+                            callback2(null, res)
+                        })
+                        .catch(callback2)
+                }
+            }), 10)
+            .then(() => {
+                callback()
+            })
+            .catch(callback)
+        }
+    }), 10)
+    await tasks;
+})()
